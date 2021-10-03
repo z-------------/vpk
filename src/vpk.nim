1c43509012ab1a1d48986d42a1f6d94dfcf22f71
Zack Guard <zacharyguard@gmail.com>
2021-10-03 07:14:15 +0000

    Rename parse.nim -> read.nim

--- src/vpk.nim
+++ src/vpk.nim
@@ -13,10 +13,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import vpkpkg/parse
+import vpkpkg/read
 import std/os
 
-export parse
+export read
 
 when isMainModule:
   doAssert paramCount() >= 1
